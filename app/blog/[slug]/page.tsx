b099617d3d3d331be5b748f48df704596d30a2bd
Edantuti <edantuti@gmail.com>
2024-11-04 15:19:37 +0530

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,32 +1,31 @@
+import { readdirSync, readFileSync } from "fs";
+import * as matter from "gray-matter";
+import { notFound } from "next/navigation";
+import { join } from "path";
+import markdownit from "markdown-it";
+import { BLOG_PATH } from "@/app/constant";
 
-import { readdirSync, readFileSync } from "fs"
-import * as matter from "gray-matter"
-import { notFound } from "next/navigation"
-import { join } from "path"
-import markdownit from "markdown-it"
-import { BLOG_PATH } from "@/app/constant"
+const getBlogBySlug = (slug: string) => {
+  const fileName: string = readdirSync(BLOG_PATH)
+    .map((path) => path.replace(/\.mdx?$/, ""))
+    .filter((name: string) => name === slug)[0];
+  if (!fileName) return null;
 
-const getBlogBySlug = (slug:string)=>{
-  const fileName:string = readdirSync(BLOG_PATH).map((path)=>path.replace(/\.mdx?$/, '')).filter((name:string)=>name===slug)[0]
-  if(!fileName)
-    return null 
+  const fileData = matter.read(join(BLOG_PATH, `${fileName}.mdx`));
+  const md = markdownit({ html: true, linkify: true });
 
-  const fileData = matter.read(join(BLOG_PATH, `${fileName}.mdx`))
-  const md = markdownit({html:true,linkify:true})
-  
-  return {data:fileData.data, content:md.render(fileData.content)}
-}
+  return { data: fileData.data, content: md.render(fileData.content) };
+};
 
-
-export default async function Blog({params}:{params:{slug:string}}){
-    const article = getBlogBySlug(decodeURI(params.slug)) 
-    if(!article){
-      notFound()
-    }
-    return (            
-      <section dangerouslySetInnerHTML={{__html:article.content}} className="xl:mx-40 lg:mx-28 md:mx-10 mx-5 pt-20 text-white prose prose-headings:text-white prose-headings:font-mono prose-p:text-white">
-      
-
-      </section>
-    )
+export default async function Blog({ params }: { params: { slug: string } }) {
+  const article = getBlogBySlug(decodeURI(params.slug));
+  if (!article) {
+    notFound();
+  }
+  return (
+    <section
+      dangerouslySetInnerHTML={{ __html: article.content }}
+      className="xl:mx-40 lg:mx-28 md:mx-10 mx-5 pt-20 text-white prose prose-headings:text-white prose-headings:font-sans prose-p:text-white"
+    ></section>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,16 @@ import { BLOG_PATH } from "@/app/constant";
 
 function getAllBlogs() {
   const article = [];
-  const fileName: string[] = readdirSync(BLOG_PATH).filter((path) => path.includes('.mdx'));
+  const fileName: string[] = readdirSync(BLOG_PATH).filter((path) =>
+    path.includes(".mdx"),
+  );
+  console.log(fileName);
   for (let file in fileName) {
-    let fda = matter.read(join(BLOG_PATH, fileName[file]));
-    article.push({ filename: fileName[file].replace(/\.mdx?$/, ""), ...fda });
+    let fileData = matter.read(join(BLOG_PATH, fileName[file]));
+    article.push({
+      filename: fileName[file].replace(/\.mdx?$/, ""),
+      ...fileData,
+    });
   }
   article.sort((a, b) => {
     if (a.data.time > b.data.time) return -1;
@@ -26,11 +32,11 @@ export default function Home() {
       <h2 className="text-xl font-semibold">Welcome to the blog</h2>
       <section className=" space-y-4">
         <h3 className="text-base font-semibold">Latest blog</h3>
-        {blog.length===0 && 
-            (<article className="min-h-32 drop-shadow-md hover:bg-gray-800 rounded py-6 space-y-2 px-2 transition-colors">
-              No Latest Blogs
-              </article>)
-        }
+        {blog.length === 0 && (
+          <article className="min-h-32 drop-shadow-md hover:bg-gray-800 rounded py-6 space-y-2 px-2 transition-colors">
+            No Latest Blogs
+          </article>
+        )}
 
         {blog.length > 0 && (
           <Link href={`blog/${blog[0].filename}`}>
@@ -50,11 +56,11 @@ export default function Home() {
       </section>
       <section className="space-y-4">
         <h3 className="text-base font-semibold">Blogs</h3>
-        {blog.length===0 && 
-            (<article className="min-h-32 drop-shadow-md hover:bg-gray-800 rounded py-6 space-y-2 px-2 transition-colors">
-              No Blogs
-              </article>)
-        }
+        {blog.length === 0 && (
+          <article className="min-h-32 drop-shadow-md hover:bg-gray-800 rounded py-6 space-y-2 px-2 transition-colors">
+            No Blogs
+          </article>
+        )}
         {blog.length > 0 &&
           blog.map(({ filename, data }) => (
             <Link key={data.title} href={`blog/${filename}`}>
